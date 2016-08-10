ceefa981f2bf4b73fb7692e92353ff7641092120
Michael Fosco <mfosco55@gmail.com>
2016-08-09 22:32:31 -0400

    Added some comments

--- HelloLesson/hello.cpp
+++ HelloLesson/hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+// Basic Hello World program
 int main()
 {
     std::cout << "Hello world!" << std::endl;
+    std::cout << "It is very nice to meet you!" << std::endl;
+    /* This is a mulit-line
+     * comment */
     return 0;
 }
