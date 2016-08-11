cb474a6f593156161f0b9a78ace96a34955484a5
Michael Fosco <mfosco55@gmail.com>
2016-08-10 21:59:29 -0400

    Some functions (1.4 lesson)

--- HelloLesson/hello.cpp
+++ HelloLesson/hello.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
+void doPrint() {
+    std::cout << "In doPrint\n";
+}
+
+//Add 2 numbers together
+int add(int x, int y) {
+    return x + y;
+}
+
 // Basic Hello World program
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
-    std::cout << "It is very nice to meet you!" << std::endl;
+    using std::cout; //declaration tells compiler that cout should resolve to std::cout
+    cout << "Hello world!" << std::endl;
+    cout << "It is very nice to meet you!" << std::endl;
+    int x = 4;
+    cout << "The value of x is: " << x;
+    cout << "\nDoes this \n split?\n";
+    //cout << "Enter value for x: ";
+    //read input from user:
+    doPrint();
+    //std::cin >> x;
+    //cout << "You entered: " << x << "\n";
     /* This is a mulit-line
      * comment */
     return 0;
